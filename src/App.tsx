ace8177f5e1fd5a9a014b087e119aba4ee0665ff
edubaptista7 <eduardofbaptista@gmail.com>
2020-07-15 16:21:56 -0300

    chore: add react-router-dom context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from 'styles/global';
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
       <h2>peçaZap</h2>
-    </>
+    </BrowserRouter>
   );
 };
 
